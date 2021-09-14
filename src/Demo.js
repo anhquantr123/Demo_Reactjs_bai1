0b61695fa4d3382f2f6c52ed9554b9e6b6a066c3
NGUYỄN VĂN QUÂN <anhquan.lien123@gmail.com>
2021-09-14 22:58:23 +0700

    add components

---
+++ src/Demo.js
@@ -0,0 +1,15 @@
+
+function Demo() {
+    return (
+        <div>
+            <h1>
+                welcome to reactjs
+            </h1>
+            <p>
+                chào mừng đến với bài học đầu tiên trên react js
+            </p>
+        </div>
+    );
+}
+
+export default Demo;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import Demo from './Demo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+    <Demo/>
   </React.StrictMode>,
   document.getElementById('root')
 );
